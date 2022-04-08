e6d6e2d481ac2d93c23f636266de17e56d6b291e
Mathieu Barbin <mathieu.barbin@gmail.com>
2022-04-08 12:41:17 +0200

    upgrade to ocaml 4.13

--- src/import.ml
+++ src/import.ml
@@ -3,10 +3,5 @@ open! Core
 module Float = struct
   include Float
 
-  let log2 =
-    (* This function is available as [Float.log2] from OCaml 4.13. To be
-     simplified upon upgrading to a more recent compiler. *)
-    let log2 = lazy (Caml.Float.log 2.) in
-    fun x -> Caml.Float.log x /. Lazy.force log2
-  ;;
+  let log2 = Caml.Float.log2
 end
---
+++ src/import.mli
@@ -0,0 +1,12 @@
+open! Core
+
+module Float : sig
+  include module type of struct
+    include Float
+  end
+
+  (** This function is available as [Caml.Float.log2] from OCaml 4.13
+     but hasn't made it to [Core.Float] yet. To be simplified when it
+     does. *)
+  val log2 : t -> t
+end
