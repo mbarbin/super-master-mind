21f72b28ed972ce4225caa73f4ac9c2f84e2eb53
Mathieu Barbin <mathieu.barbin@gmail.com>
2023-10-25 14:02:15 +0200

    updated test

--- lib/super_master_mind/test/test__game_dimensions.ml
+++ lib/super_master_mind/test/test__game_dimensions.ml
@@ -1,28 +1,27 @@
 open! Core
-open! Core
 open Super_master_mind
 
 let%expect_test "set twice" =
+  Game_dimensions.use_small_game_dimensions_exn [%here];
   Expect_test_helpers_core.require_does_raise [%here] (fun () ->
-    Game_dimensions.use_small_game_dimensions_exn [%here];
     Game_dimensions.use_small_game_dimensions_exn [%here]);
   [%expect
     {|
     ("Game_dimensions is already set"
      lib/super_master_mind/test/test__game_dimensions.ml:7:50
-     ((was_set_here lib/super_master_mind/src/cue.ml:12:48))) |}]
+     ((was_set_here lib/super_master_mind/test/test__game_dimensions.ml:5:48))) |}]
 ;;
 
 let%expect_test "set after use" =
+  print_s [%sexp { code_size = (Game_dimensions.code_size [%here] : int) }];
   Expect_test_helpers_core.require_does_raise [%here] (fun () ->
-    print_s [%sexp { code_size = (Game_dimensions.code_size [%here] : int) }];
     Game_dimensions.use_small_game_dimensions_exn [%here]);
   [%expect
     {|
-    ((code_size 5))
+    ((code_size 3))
     ("Game_dimensions is already set"
-     lib/super_master_mind/test/test__game_dimensions.ml:19:50
-     ((was_set_here lib/super_master_mind/src/cue.ml:12:48))) |}]
+     lib/super_master_mind/test/test__game_dimensions.ml:18:50
+     ((was_set_here lib/super_master_mind/test/test__game_dimensions.ml:5:48))) |}]
 ;;
 
 let%expect_test "defaults" =
@@ -31,5 +30,5 @@ let%expect_test "defaults" =
       { code_size = (Game_dimensions.code_size [%here] : int)
       ; num_colors = (Game_dimensions.num_colors [%here] : int)
       }];
-  [%expect {| ((code_size 5) (num_colors 8)) |}]
+  [%expect {| ((code_size 3) (num_colors 4)) |}]
 ;;
