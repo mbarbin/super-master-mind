996cc3ed72ea653795b15faaff754158030e1d43
Mathieu Barbin <mathieu.barbin@gmail.com>
2024-06-01 11:34:16 +0200

    Use [Base.Float.log2] (available >= v0.17.0)

--- lib/super_master_mind/src/import/super_master_mind_import.ml
+++
@@ -1,5 +0,0 @@
-module Float = struct
-  include Float
-
-  let log2 = Stdlib.Float.log2
-end
--- lib/super_master_mind/src/import/super_master_mind_import.mli
+++
@@ -1,9 +0,0 @@
-module Float : sig
-  include module type of struct
-    include Float
-  end
-
-  (** This function is available as [Stdlib.Float.log2] from OCaml 4.13 but
-      hasn't made it to [Core.Float] yet. To be simplified when it does. *)
-  val log2 : t -> t
-end
