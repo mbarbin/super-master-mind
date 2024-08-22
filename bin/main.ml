c525891bece0bf3670b9cf4f68274beb1f313b3a
Mathieu Barbin <mathieu.barbin@gmail.com>
2024-08-22 12:20:24 +0200

    Upgrade to commandlang.0.0.1

--- bin/main.ml
+++ bin/main.ml
@@ -1,8 +1,6 @@
 let () =
-  Cmdliner.Cmd.eval
-    (Commandlang_to_cmdliner.Translate.command
-       Super_master_mind.main
-       ~name:"super-master-mind"
-       ~version:"%%VERSION%%")
-  |> Stdlib.exit
+  Commandlang_to_cmdliner.run
+    Super_master_mind.main
+    ~name:"super-master-mind"
+    ~version:"%%VERSION%%"
 ;;
