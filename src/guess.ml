d877a6fb318c5da51274bf9304170174a45243ce
Mathieu Barbin <mathieu.barbin@gmail.com>
2026-01-18 16:38:57 +0100

    Another experiment with local bindings

--- src/guess.ml
+++ src/guess.ml
@@ -391,10 +391,9 @@ let to_json = T.to_json
 let of_json = T.of_json
 
 let compute ~possible_solutions ~candidate : t =
-  let module Float = Base.Float in
-  let ( +. ) = Base.( +. ) in
-  let ( *. ) = Base.( *. ) in
-  let ( /. ) = Base.( /. ) in
+  let ( +. ) = Stdlib.( +. ) in
+  let ( *. ) = Stdlib.( *. ) in
+  let ( /. ) = Stdlib.( /. ) in
   if Codes.is_empty possible_solutions then Code_error.raise "No possible solutions." [];
   let original_size = Float.of_int (Codes.size possible_solutions) in
   let original_bits = Float.log2 original_size in
