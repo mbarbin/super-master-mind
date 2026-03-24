851bd65491f06209d79db6333c6a81b90dcfe154
Mathieu Barbin <mathieu.barbin@gmail.com>
2026-03-24 10:42:26 +0100

    Use dune-build-info for --version

--- src/bin/main.ml
+++ src/bin/main.ml
@@ -4,9 +4,15 @@
 (*  SPDX-License-Identifier: MIT                                                 *)
 (*********************************************************************************)
 
+let version =
+  match Build_info.V1.version () with
+  | None -> "n/a"
+  | Some v -> Build_info.V1.Version.to_string v
+;;
+
 let () =
   Cmdlang_cmdliner_err_runner.run
     Super_master_mind.main
     ~name:"super-master-mind"
-    ~version:"%%VERSION%%"
+    ~version
 ;;
