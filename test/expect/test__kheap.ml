6f78cb64d92ec113a145688fe653a998a42877fb
Mathieu Barbin <mathieu.barbin@gmail.com>
2026-09-16 13:44:44 +0200

    Add kheap characterization

--- test/expect/test__kheap.ml
+++ test/expect/test__kheap.ml
@@ -25,3 +25,27 @@ let%expect_test "kheap" =
   print ();
   [%expect {| [ 2; 3; 4 ] |}]
 ;;
+
+(* To observe how values that compare equal are ordered, we need values that
+   remain distinguishable once compared. We use pairs that are compared on
+   their first component only, and tag their second component. *)
+let%expect_test "equal values" =
+  let h = Kheap.create ~k:3 ~compare:(fun (i, _) (j, _) -> Int.compare i j) in
+  let print () =
+    print_dyn (Kheap.to_dyn (fun (i, tag) -> Dyn.Tuple [ Dyn.int i; Dyn.string tag ]) h)
+  in
+  (* Values that compare equal are kept in the order in which they were added. *)
+  Kheap.add h (1, "a");
+  Kheap.add h (1, "b");
+  print ();
+  [%expect {| [ (1, "a"); (1, "b") ] |}];
+  (* A smaller value takes its place before them. *)
+  Kheap.add h (0, "c");
+  print ();
+  [%expect {| [ (0, "c"); (1, "a"); (1, "b") ] |}];
+  (* Now that the heap is full, a value that compares equal to the ones already
+     present would go after them, and is thus discarded. *)
+  Kheap.add h (1, "d");
+  print ();
+  [%expect {| [ (0, "c"); (1, "a"); (1, "b") ] |}]
+;;
